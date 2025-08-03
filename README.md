80168fbbe0cc60e64533bf928e2ff1f64a434a69
shivasharan2023 <shivasharan.k215@gmail.com>
2025-08-03 19:13:12 +0530

    logging and exception

---
+++ README.md
@@ -0,0 +1,1 @@
+End-to_End Machine Learning Project
--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ def get_requirements(file_path:str) -> List[str]:
     This function will return the list of requirements
     '''
     requirements = []
-    with open(file_path) as file_object:
-        requirements=file_object.readlines()
-        requirements=[req.replace("/n", " " )for req in requirements]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", " " )for req in requirements]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
@@ -17,8 +17,8 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
 name = 'End-to-End_ML_Project',
 version = '0.01',
-author = 'Shiva',
-author_email='s.kalemakal@xx.com',
+author = 'Shivasharan',
+author_email='shivasharan.k215@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt') 
 )
