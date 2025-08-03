623b44dc51ae39f46f3454bb4d59463e855b777b
shivasharan2023 <shivasharan.k215@gmail.com>
2025-08-03 17:11:54 +0530

    setp

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_object:
+        requirements=file_object.readlines()
+        requirements=[req.replace("/n", " " )for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+name = 'End-to-End_ML_Project',
+version = '0.01',
+author = 'Shiva',
+author_email='s.kalemakal@xx.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt') 
+)
